e5560e838f401d17df6c1d5ab8cfb703dc16bfb9
mbs7works <39623235+mbs7works@users.noreply.github.com>
2018-07-25 16:29:23 +0530

    HttpInterceptor

--- analytics-new/src/app/common/httpInterceptor.ts
+++ analytics-new/src/app/common/httpInterceptor.ts
@@ -5,26 +5,43 @@ import {
   HttpHandler,
   HttpEvent,
   HttpResponse,
+    HttpErrorResponse
 } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
+import { BaseConfig } from './base.config';
 
 @Injectable()
 export class HttpLogoutInterceptor implements HttpInterceptor {
+    private baseConfig: BaseConfig;
+    constructor(){
+        this.baseConfig = new BaseConfig();
+    }
+
+   
   intercept(
     request: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
-
-    
-
     let hreq = request.clone({
         setHeaders:{
           'authorization' : 'bearer'
         }
     });
+    console.log(hreq);
+      return next.handle(request).do((event: HttpEvent<any>) => {
+          if (event instanceof HttpResponse) {
+           console.log('processing response',event);
+          }
+      }, (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+              if (err.status === 302) {
+                 const url = this.baseConfig.goToProfile();
+                window.location.href = url;
+                  
+              }
+          }
+      });
 
-    return next.handle(hreq)
-    
 
   }
 }
